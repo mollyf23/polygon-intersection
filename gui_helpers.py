17d307f4deacea135002c6428b81f7cfd53c662e
mollyf23 <mollyfawcett23@gmail.com>
2024-11-22 01:26:15 -0500

    new helpers

--- gui_helpers.py
+++ gui_helpers.py
@@ -1,4 +1,5 @@
 from matplotlib.axes import Axes
+import matplotlib.pyplot as plt
 from Point import Point
 from Segment import Segment
 import matplotlib.pyplot as plt
@@ -19,10 +20,29 @@ def draw_segment(ax: Axes, segment: Segment, color="red", linewidth=1):
     ax.figure.canvas.flush_events()  
     plt.pause(1)  
 
-def draw_polygon(ax: Axes, poly_points):
+
+def draw_polygon(ax: Axes, poly_points, color="black"):
     if poly_points:
         x1, y1 = zip(*poly_points)
         ax.plot(x1 + (x1[0],), y1 + (y1[0],), marker='o')
         ax.figure.canvas.draw()  
         ax.figure.canvas.flush_events()  
         plt.pause(1)  
+
+#for shamos hoey
+def draw_intersection_point(ax, event, segE, segA):
+    draw_point(ax, event.point,color="yellow")
+    draw_segment(ax, segE, color="yellow")
+    draw_segment(ax, segA, color="yellow")
+    #todo: cut off chunk of segment
+    return
+
+def draw_left_endpoint(ax, event):
+    draw_point(ax, event.point)
+    draw_segment(ax, event.segment)
+    return
+
+def draw_right_endpoint(ax, event):
+    draw_point(ax, event.point)
+    #todo: erase segment
+    return
